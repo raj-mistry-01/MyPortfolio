82f70096a351745aaf3067f9628c4f24773e03b7
Makwana Raj <151895608+raj-mistry-01@users.noreply.github.com>
2025-05-13 01:31:16 +0530

    8th

--- portfolio/data/projects.ts
+++ portfolio/data/projects.ts
@@ -4,26 +4,24 @@ export const projects: Project[] = [
   {
     id: "1",
     title: "PipeTest",
-    description: "A full-stack e-commerce platform with product listings, cart functionality, and secure checkout.",
+    description: "A System Testing Tool for Developers , Pull Request validation using GitLab CI/CD Pipeline.",
     technologies: ["Flask" , "GitLab CI/CD" , "Selenium" , "Gemini Api" , "React" , "MongoDB" , "Node JS" , "PowerShell Scripting"],
     githubUrl: "https://github.com/username/ecommerce",
-    liveUrl: "https://ecommerce-demo.vercel.app",
+    // liveUrl: "https://ecommerce-demo.vercel.app",
     image : "/images/Stest.png"
   },
   {
     id: "2",
     title: "Now Ev",
-    description:
-      "A Kanban-style task management application with drag-and-drop functionality and team collaboration features.",
+    description: "Eletronic Vehicle Rental Management , CRM platform for company , E-Kyc And Secure Payment Gateway",
     technologies: ["Flutter" , "MongoDB" , "OpenCV" , "Flask" , "NodeJS" , "CrossPlatform"],
     githubUrl: "https://github.com/username/task-manager",
-    liveUrl: "https://task-manager-demo.vercel.app",
+    // liveUrl: "https://task-manager-demo.vercel.app",
   },
   {
     id: "3",
     title: "Environmental Assessment",
-    description:
-      "A weather dashboard that displays current conditions and forecasts for multiple locations using a weather API.",
+    description: "Environmental Effect Measurement Due To Construction  , Comprehensive report generation demonstaring effective solution",
     technologies: ["Machine Learning" , "React" , "Flask" , "GoogleEarthEngine" , "Taillwind"],
     githubUrl: "https://github.com/username/weather-dashboard",
     image : "/images/Nasa.jpeg"
@@ -31,16 +29,16 @@ export const projects: Project[] = [
   {
     id: "4",
     title: "FinTaxAi",
-    description: "A dashboard for tracking and visualizing social media metrics across multiple platforms.",
+    description: "Financial Assistant , Daily transaction management , Chatbot for help , Tax Payment Report automated generation.",
     technologies: ["Flask" , "NodeJS" , "MongoDB" , "React" , "Tailwind" , "OCR" , "Gemini"],
     githubUrl: "https://github.com/username/social-analytics",
-    liveUrl: "https://social-analytics-demo.vercel.app",
+    // liveUrl: "https://social-analytics-demo.vercel.app",
     image : "/images/FinTaxAi.jpeg"
   },
   {
     id: "5",
     title: "Weather App",
-    description: "An application to track expenses, income, and financial goals with data visualization.",
+    description: "Desktop application , accurate weather , history of searched location , dynamic UI according weather condition.",
     technologies: ["Java" , "Java Swing" , "Open MeteoApi" , "OpenJDK" , "Jar"],
     githubUrl: "https://github.com/username/finance-tracker",
     image : "/images/Weather.avif"
@@ -48,11 +46,10 @@ export const projects: Project[] = [
   {
     id: "6",
     title: "iNoteBook",
-    description:
-      "A community-driven platform for sharing and discovering recipes with search and filtering capabilities.",
+    description: "Digital platform for notes , pdf supported for download",
     technologies: ["Flask", "NodeJS" , "JWT" , "React"],
     githubUrl: "https://github.com/username/recipe-platform",
-    liveUrl: "https://recipe-platform-demo.vercel.app",
+    // liveUrl: "https://recipe-platform-demo.vercel.app",
     image : "/images/in2.png"
   },
 ]
